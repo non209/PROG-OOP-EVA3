2bc317887b929278dfb330fdb70468dca0f243d9
non209 <143562722+non209@users.noreply.github.com>
2024-04-22 17:51:19 -0700

    Add files via upload

---
+++ EVA3_1_EXEPCIONES/src/eva3_1_exepciones/EVA3_1_EXEPCIONES.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_1_exepciones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_1_EXEPCIONES {
+
+    /**ñalkdsjñaskdjlñskdfjñlaskdfñalksdjñlaksjdfñasñaklsdfjañksdfñañkfjññakdjf
+     *sd}ojfffdskdjfdsñfkfhola que hace jdfslkdjfsñdkfjjaja fhfdskfsadsffsdffdf
+     * sldakfaslkdjfaslkdjflsdkfslkdfjfksldfklfdsñadkjfslkajfkdlsañsldkfjsdlfkj
+     * ssañlkdjflsaslkdjljflkkdsklskkjdflskdfldskfjlkfdskfdskdkdkdkjsksldkfjdsl
+     * dfkkjdsljkfdslkjfdsdkjflsdskjsdkfjkjdsfkldsfjksdfdldfsldlkdfslkjsdfjlksd
+     * kjfdskjdfskfdjkdfjjksdfkdfkj{ñoe´pwkdñslkfkdjlljsfdsñlhgdfffslkjhfjdkslj
+     * blfdkslkfjslfdgnnvmdflvmdlfkgjfdjkfg{laoierrrrvmmmmmmmmmmmmodooooooooooo
+     * dfgsdfgdfsgsdfgfdshijhijrsiotjhrrrrrrrrjhrijhfgiiiiiiiiijrtohjrpotjphroj
+     * hriffh
+     * @param args the command line argumentssldkfjlslkdjfañlkjdfalkdjfsaldkfsdl
+     */
+    public static void main(String[] args) {
+
+        int num;
+        Scanner sc= new Scanner (System.in);
+        System.out.println("introduce un valor entero");
+        num = sc.nextInt ();
+        System.out.println("El valor es " + num);
+       
+    }
+    
+}
